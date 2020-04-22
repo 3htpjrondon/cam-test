9bc0da78a36acc0ff90d8907f032a69c8df1b19a
3htpjrondon <64159854+3htpjrondon@users.noreply.github.com>
2020-04-22 12:56:54 -0500

    initial upload

---
+++ variables.tf
@@ -0,0 +1,12 @@
+#####################################################################
+##
+##      Creado el 22/04/20 por admin. para aws-env-test
+##
+#####################################################################
+
+variable "bucket" {
+  type = "string"
+  description = "bucket name"
+  default = "3htp-cam-test-bucket"
+}
+
